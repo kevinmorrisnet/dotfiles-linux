1b7a72af4711e590a51cbb0cb3ac863b9f0e52cc
kevinmorrisnet <kevinmorrisone@icloud.com>
2026-01-31 17:17:46 -0500

    fix: add .gitconfig

--- .chezmoiscripts/run_once_install-packages.sh
+++ .chezmoiscripts/run_once_install-packages.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-#sudo apt install alacritty
+# Alacritty
+sudo apt install alacritty
+# curl
 sudo apt install curl
-sudo apt install devpod
+#Alacritty Themes
 git clone git@github.com:alacritty/alacritty-theme.git
+# Nerdfonts
+curl -fsSL https://raw.githubusercontent.com/zy0r/nerd-fonts-installer/main/install_nerd_fonts.sh | bash
+# Devpod
 curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64" && sudo install -c -m 0755 devpod /usr/local/bin && rm -f devpod
