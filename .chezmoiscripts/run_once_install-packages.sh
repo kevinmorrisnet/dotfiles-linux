02623971b9d68fb0e08251b5bb102d4db26a3b93
kevinmorrisnet <kevinmorrisone@icloud.com>
2026-03-07 17:52:33 -0500

    terraform add config.toml, add az install curl to run_once .chezmoiscripts

--- .chezmoiscripts/run_once_install-packages.sh
+++ .chezmoiscripts/run_once_install-packages.sh
@@ -9,3 +9,5 @@ git clone git@github.com:alacritty/alacritty-theme.git
 curl -fsSL https://raw.githubusercontent.com/zy0r/nerd-fonts-installer/main/install_nerd_fonts.sh | bash
 # Devpod
 curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64" && sudo install -c -m 0755 devpod /usr/local/bin && rm -f devpod
+#azure CLI
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
